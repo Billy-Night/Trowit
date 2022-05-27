5a10f4495d2fc436099bfc8904e8246d7ca8305b
jessspo <67518904+jessspo@users.noreply.github.com>
2022-05-27 14:24:56 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import Navbar from './components/Navbar';
+
 
 function App() {
+  // const context = useContext(MyContext);
+
   return (
-    <div className="App">hello jess
-    </div>
+    <Router>
+    <Navbar />
+    <Routes>
+      
+    </Routes>
+  </Router>
   );
 }
 
