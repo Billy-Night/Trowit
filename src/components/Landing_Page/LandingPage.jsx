8bf432ed1c01871536d17f194f8ac0c9f3d42107
Billy <billy.m.nightingale@gmail.com>
2022-05-30 13:51:04 +0200

    checked the the code for landing page, all is good

--- src/components/Landing_Page/LandingPage.jsx
+++ src/components/Landing_Page/LandingPage.jsx
@@ -1,7 +1,7 @@
 import TopNavbar from '../TopNavBar/TopNavbar.jsx';
 import HeroSection from './Hero_Section/HeroSection.jsx'
-import ImpressYourPeers from '../Landing_Page/Impress_your_peers_section/ImpressYourPeers';
-import LandingPageFeatures from '../Landing_Page/Features_section/LandingPageFeatures';
+import ImpressYourPeers from './Impress_your_peers_section/ImpressYourPeers.jsx';
+import LandingPageFeatures from '../Landing_Page/Features_section/LandingPageFeatures.jsx';
 
 const LandingPage = () => {
 
