8a928d5e2aca6561febf2920df903b47ae3fa618
Billy <billy.m.nightingale@gmail.com>
2022-05-31 20:40:36 +0200

    added side navbar component and css file, render in cards and contacts

--- src/components/Contacts_Page/Contacts.jsx
+++ src/components/Contacts_Page/Contacts.jsx
@@ -1,9 +1,11 @@
+import SideNavBar from '../Side_NavBar/SideNavBar.jsx';
 import './Contacts.css';
 
 const Contact = () => {
 
     return (
         <div className='contact'>
+            <SideNavBar />
             <p>This is the contacts page</p>
         </div>
     );
