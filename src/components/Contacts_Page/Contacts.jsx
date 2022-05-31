6e1b801c2fdc6068d7ad48eda529078f5b05c3f7
Billy <billy.m.nightingale@gmail.com>
2022-05-31 20:30:45 +0200

    added contacts component and created css file, added to the routes on app.js

---
+++ src/components/Contacts_Page/Contacts.jsx
@@ -0,0 +1,12 @@
+import './Contacts.css';
+
+const Contact = () => {
+
+    return (
+        <div className='contact'>
+            <p>This is the contacts page</p>
+        </div>
+    );
+}
+
+export default Contact;
