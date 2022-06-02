1f3bb9d6fea4d21e39c79f3d6b0a58811220e8e6
BRYAN BLAIR <bryan.clawd.blair@gmail.com>
2022-06-02 14:48:18 +0200

    Worked on the Impress Your Peers section and added images.

--- src/components/Landing_Page/Impress_your_peers_section/ImpressYourPeers.jsx
+++ src/components/Landing_Page/Impress_your_peers_section/ImpressYourPeers.jsx
@@ -1,9 +1,16 @@
+import profile_img from '../../../images/landing_page/profile_img@2x.png';
 
 const ImpressYourPeers = () => {
 
     return (
         <div>
-            <p>This is the impress your peers section</p>
+             <img
+          src={profile_img}
+          class=""
+          alt="Profile image"
+        />
+            <p>Create your beautifully designed Digital Business Card with just a few clicks, and easily share it through email, social media and more.</p>
+            <p>Anyone can receive your digital card, even if they don't have an account on TROWIT!</p>
         </div>
     );
 };
