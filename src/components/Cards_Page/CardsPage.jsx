ef41ae9674090741ee856adb7c7284f49379059d
Moran <thaler.moran@gmail.com>
2022-06-14 14:25:39 +0200

    srat to work on the cards sections

--- src/components/Cards_Page/CardsPage.jsx
+++ src/components/Cards_Page/CardsPage.jsx
@@ -1,13 +1,29 @@
-import SideNavBar from "../Side_NavBar/SideNavBar.jsx";
+import './CardsPage.css';
+import SideNavBar from '../Side_NavBar/SideNavBar.jsx';
+import cardWork from '../../images/cards_page/card_work@2x.png';
+import cardPersonal from '../../images/cards_page/card_personal@2x.png';
+import createCard from '../../images/cards_page/create_card@2x.png';
 
 const CardsPage = () => {
-
-    return (
-        <div>
-            <SideNavBar />
-            <p>This is the cards page</p>
+  return (
+    <div id="cardsPage">
+      <div>
+        <SideNavBar />
+        <p>This is the cards page</p>
+      </div>
+      <div className="CP_cards">
+        <div className="card">
+          <img src={cardWork} alt="card for work" />
         </div>
-    );
+        <div className="card">
+          <img src={cardPersonal} alt="card for personal stuff" />
+        </div>
+        <div className="card">
+          <img src={createCard} alt="create a card" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default CardsPage; 
+export default CardsPage;
