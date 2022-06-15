e8d0c44ee12b6def032a619466ed0e2a20681bb7
jessspo <67518904+jessspo@users.noreply.github.com>
2022-06-15 15:25:53 +0200

    search bar styled

--- src/components/Cards_Page/SearchCard.jsx
+++ src/components/Cards_Page/SearchCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./SearchCard.css"
 
 const SearchCard = ({keyword,setKeyword}) => {
 
-    const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem", margin: "2%", font: 'Nunito'};
+    // const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem", margin: "2%", font: 'Nunito'};
     return (
       <input 
-       style={BarStyling}
+       className="barStyling"
        key="random1"
        value={keyword}
        placeholder={"Search Card"}
