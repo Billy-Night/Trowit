95361f850d35d4c4757e58928dcfb3fc632005fd
Moran <thaler.moran@gmail.com>
2022-06-16 19:32:12 +0200

    added the scroll feature with 2 files in the hero section to the folder.

--- src/components/Landing_Page/Hero_Section/HeroSection.jsx
+++ src/components/Landing_Page/Hero_Section/HeroSection.jsx
@@ -1,14 +1,12 @@
 import './HeroSection.css';
 import heroImg from '../../../images/landing_page/hero-img@2x.png';
+import Scroll_Down_Arrow from './Scroll_Down_Arrow';
 //The useNavigate hook returns a function that lets you navigate programmatically.
 
-
-import  { ScrollWithOffset } from '../../../utils/utils.jsx';
-import { HashLink } from "react-router-hash-link";
-
+import { ScrollWithOffset } from '../../../utils/utils.jsx';
+import { HashLink } from 'react-router-hash-link';
 
 const HeroSection = (props) => {
-
   return (
     <div id="hero-section">
       <div className="heading-primary-main">
@@ -22,15 +20,26 @@ const HeroSection = (props) => {
 
       {/* <C2A-btn />
             <Learn-more-btn /> */}
-      <button className="C2A--btn" onClick={props.HandleClickOrdCard} > Order your TROWIT Card</button>
+      <button className="C2A--btn" onClick={props.HandleClickOrdCard}>
+        {' '}
+        Order your TROWIT Card
+      </button>
       {/* <button className="more--btn hover-underline">Learn More</button> */}
-      <HashLink to='#section-features1' smooth className="more--btn hover-underline" scroll={el => ScrollWithOffset(el)}>Learn More</HashLink>
+      <HashLink
+        to="#section-features1"
+        smooth
+        className="more--btn hover-underline"
+        scroll={(el) => ScrollWithOffset(el)}
+      >
+        Learn More
+      </HashLink>
 
       <img
         src={heroImg}
         alt="girl, profile of business card "
         className="hero-img"
       />
+      <Scroll_Down_Arrow />
     </div>
   );
 };
