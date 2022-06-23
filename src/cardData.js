c0129cd5781a5e939a49d1716a6c88033fb4aa09
jessspo <67518904+jessspo@users.noreply.github.com>
2022-06-23 16:32:03 +0200

    trying to link image to path

--- src/cardData.js
+++ src/cardData.js
@@ -1,8 +1,17 @@
 import cardWork from '../src/images/cards_page/card_work@2x.png';
 import cardPersonal from '../src/images/cards_page/card_personal@2x.png';
 import createCard from '../src/images/cards_page/create_card@2x.png';
+import React, {Link} from 'react';
 
-const CardsData = [
+const CardsData(props) {
+    return (
+    <>
+
+    <Link to={props.path}>
+        <img className='Work'
+        alt="Work card image"
+        src="cardWork"/>
+    </Link>
     {
         "id": 1,
         "name": "Work",
@@ -15,12 +24,17 @@ const CardsData = [
         "image": cardPersonal,
         "imgAlt": "Personal card image"
     },
+
     {
+    
         "id": 2,
         "name": "Add Card",
         "image": createCard,
         "imgAlt": "Add card image"
     }
-]
+    </>
+    ); 
+}
+
 
 export default CardsData;
