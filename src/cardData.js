068856c090fedf354a2d17347e8e446ff349d9ce
jessspo <67518904+jessspo@users.noreply.github.com>
2022-06-23 17:17:58 +0200

    Cards corrected linking to a new card page

--- src/cardData.js
+++ src/cardData.js
@@ -3,26 +3,21 @@ import cardPersonal from '../src/images/cards_page/card_personal@2x.png';
 import createCard from '../src/images/cards_page/create_card@2x.png';
 import React, {Link} from 'react';
 
-const CardsData(props) {
-    return (
-    <>
-
-    <Link to={props.path}>
-        <img className='Work'
-        alt="Work card image"
-        src="cardWork"/>
-    </Link>
+const CardsData = [
+ 
     {
         "id": 1,
         "name": "Work",
         "image": cardWork,
-        "imgAlt": "Work card image"
+        "imgAlt": "Work card image",
+        "address": "#"
     },
     {
         "id": 2,
         "name": "Personal",
         "image": cardPersonal,
-        "imgAlt": "Personal card image"
+        "imgAlt": "Personal card image",
+        "address": "#"
     },
 
     {
@@ -30,11 +25,9 @@ const CardsData(props) {
         "id": 2,
         "name": "Add Card",
         "image": createCard,
-        "imgAlt": "Add card image"
-    }
-    </>
-    ); 
-}
-
+        "imgAlt": "Add card image",
+        "address": "/cards/newcard"
+    } 
+]
 
 export default CardsData;
