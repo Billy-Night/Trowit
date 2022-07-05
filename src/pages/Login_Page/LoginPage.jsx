be0eb05b3060e21ef6efebe7ff45eea82f943b25
Billy <billy.m.nightingale@gmail.com>
2022-07-05 16:41:56 +0200

    added login api call, everything is working correctly

--- src/pages/Login_Page/LoginPage.jsx
+++ src/pages/Login_Page/LoginPage.jsx
@@ -1,25 +1,38 @@
-//import React, { useContext } from "react";
+import React, { useContext } from "react";
 import './LoginPage.css';
 import trowit_logo_img from '../../images/landing_page/logo_small@4x.png';
 import google_sign_up from '../../images/landing_page/google_sign_in.png';
-//import { MyContext } from '../../context/MyProvider';
+import { MyContext } from '../../context/MyProvider';
+import { useNavigate } from "react-router-dom";
 
 
 const LoginPage = () => {
-  // const context = useContext(MyContext);
+  const navigate = useNavigate();
+  const context = useContext(MyContext);
 
-  // const handleLogin = (event) => {
-  //   event.preventDefault();
-  //   fetch("http://localhost:3306/log", {
-  //     method: "POST",
-  //     headers: new Headers({
-  //       "Content-Type": "application/json",
-  //     }),
-  //     body: JSON.stringify({
-  //       email: 
-  //     })
-  //   })
-  // }
+  const handleLogin = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3306/log", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        email: context.user.email,
+        hash_password: context.user.hash_password
+      })
+    })
+    .then((response) => {
+      if (response.status === 500) {
+        context.setUser(context.user);
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data);
+      navigate("/cards")
+    })
+  }
 
 
     return (
@@ -35,21 +48,23 @@ const LoginPage = () => {
             <div>
                 <h1 className= "text">Log in to your account</h1>
             </div>
-          <form className= "loginForm">
+          <form onSubmit={handleLogin} className= "loginForm">
               <input 
-                type="text" 
-                name="Email" 
+                value={context.user.email}
+                onChange={context.handleLogReg} 
+                name="email" 
                 placeholder="Email"
                 />
               <input 
-                type="text" 
-                name="Password" 
+                value={context.user.hash_password} 
+                onChange={context.handleLogReg}
+                name="hash_password" 
                 placeholder="Password"
                 />
-              <input 
+              <input className= "reg"
                 type="submit" 
-                value= "Login"
-                className= "reg"/>
+                value= "Submit"
+                />
           </form>
           <div className="forgotPassword">
             <a href="https://www.w3schools.com">Forgot password?</a> 
