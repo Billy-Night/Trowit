d579c054adc6e0d0e088d1131f125d1807392e43
jessspo <67518904+jessspo@users.noreply.github.com>
2022-07-11 05:37:30 +0200

    route adjusted topnavbar

--- src/components/TopNavBar/TopNavbar.jsx
+++ src/components/TopNavBar/TopNavbar.jsx
@@ -34,7 +34,7 @@ const TopNavbar = () => {
 
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
-            <Link to="/home" className="nav-links" onClick={closeMobileMenu}>
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
             </Link>
           </li>
