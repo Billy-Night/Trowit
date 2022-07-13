fa1ff528d65300224867a2df14b38db6b5f6612f
jessspo <67518904+jessspo@users.noreply.github.com>
2022-07-13 18:08:36 +0200

    contact page with pictures

--- src/pages/View_Contact_Card_Page/ContactCardDesign.jsx
+++ src/pages/View_Contact_Card_Page/ContactCardDesign.jsx
@@ -4,7 +4,9 @@ import { useNavigate } from "react-router-dom";
 import SideNavBar from "../../components/Side_NavBar/SideNavBar.jsx";
 import "./ContactCardDesign.css";
 import Avatar from "../../components/Avatar/Avatar.jsx";
-import AvatarEditor from "../../components/AvatarEditor/AvatarEditor.jsx";
+import contact_card_left from "../../images/cards_page/contact.png";
+import contact_right from "../../images/cards_page/contact_right.png";
+import contact_rightdown from "../../images/cards_page/contactcardrightdown.png";
 
 
 const ContactCard = () => {
@@ -54,158 +56,70 @@ const ContactCard = () => {
     <div id="contact-card">
       <SideNavBar />
       <h1 className="contact-card-title">Contacts/TROWIT/Contact Name</h1>
-      <span className="search-area">
-        <a href="#">
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M22.7144 13.5C23.4245 13.5 24.0001 12.8284 24.0001 12C24.0001 11.1716 23.4245 10.5 22.7144 10.5V13.5ZM0.805288 10.9393C0.303185 11.5251 0.303185 12.4749 0.805288 13.0607L8.98754 22.6066C9.48965 23.1924 10.3037 23.1924 10.8058 22.6066C11.3079 22.0208 11.3079 21.0711 10.8058 20.4853L3.5327 12L10.8058 3.51476C11.3079 2.92897 11.3079 1.97923 10.8058 1.39344C10.3037 0.807654 9.48965 0.807654 8.98754 1.39344L0.805288 10.9393ZM22.7144 10.5H1.71443V13.5H22.7144V10.5Z"
-              fill="black"
-            />
-          </svg>
-        </a>
-      </span>
+        <span className="search-area">
+            <a href="#">
+            <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                d="M22.7144 13.5C23.4245 13.5 24.0001 12.8284 24.0001 12C24.0001 11.1716 23.4245 10.5 22.7144 10.5V13.5ZM0.805288 10.9393C0.303185 11.5251 0.303185 12.4749 0.805288 13.0607L8.98754 22.6066C9.48965 23.1924 10.3037 23.1924 10.8058 22.6066C11.3079 22.0208 11.3079 21.0711 10.8058 20.4853L3.5327 12L10.8058 3.51476C11.3079 2.92897 11.3079 1.97923 10.8058 1.39344C10.3037 0.807654 9.48965 0.807654 8.98754 1.39344L0.805288 10.9393ZM22.7144 10.5H1.71443V13.5H22.7144V10.5Z"
+                fill="black"
+                />
+            </svg>
+            </a>
+        </span>
     <div>
       <Avatar />
       <div id="contact-card-left">
-        <AvatarEditor />
-        <section id="contact-card-form">
-          <form onSubmit={handleContactSubmit}>
-            <input
-              value={context.contact.image}
-              onChange={context.handleContact}
-              name="image"
-              placeholder="image"
-            />
-
-            <hr className="contact-card-form-divider" />
-
-            <input
-              value={context.contact.full_name}
-              onChange={context.handleContact}
-              name="full_name"
-              placeholder="Full Name"
-            />
-
-            <input
-              value={context.contact.title}
-              onChange={context.handleContact}
-              name="title"
-              placeholder="title"
-            />
-
-            <input
-              value={context.contact.company}
-              onChange={context.handleContact}
-              name="company"
-              placeholder="Company"
-            />
-
-            <input
-              value={context.contact.email}
-              onChange={context.handleContact}
-              name="email"
-              placeholder="Email"
-            />
-
-            <input
-              value={context.contact.phone}
-              onChange={context.handleContact}
-              name="phone"
-              placeholder="Contact"
-            />
-
-            <input
-              value={context.contact.website}
-              onChange={context.handleContact}
-              name="website"
-              placeholder="website"
-            />
-
-            <input
-              value={context.contact.linkdin}
-              onChange={context.handleContact}
-              name="linkdin"
-              placeholder="linkdin"
-            />
-
-            <input
-              value={context.contact.documents}
-              onChange={context.handleContact}
-              name="documents"
-              placeholder="documents"
-            />
-
-            <input
-              value={context.contact.files}
-              onChange={context.handleContact}
-              name="files"
-              placeholder="files"
-            />
-
-            {/* <input value={context.contact.add_date} onChange={context.handleContact} name="add_date" placeholder="Connection Date" /> */}
-
-            {/* <input value={context.contact.add_time} onChange={context.handleContact} name="add_time" placeholder="Connection Time" /> */}
-
-            <input
-              value={context.contact.contact_type}
-              onChange={context.handleContact}
-              name="contact_type"
-              placeholder="Contact Type"
-            />
-            </form>
-            </section>
+        <img src={contact_card_left} alt="contact_card" className="img" />    
             <div id="contact-card-right">
                 <h1> Tags</h1>
-                <div id="contact-card-right-grid">
-            <form onSubmit={handleContactSubmit} >
-            <input
-              value={context.contact.tag1}
-              onChange={context.handleContact}
-              name="tag1"
-              placeholder="Tag 1"
-            />
+                <form onSubmit={handleContactSubmit} id="contact-card-right-grid">
+                    <input
+                    value={context.contact.tag1}
+                    onChange={context.handleContact}
+                    name="tag1"
+                    placeholder="Tag 1"
+                    />
 
-            <input
-              value={context.contact.tag2}
-              onChange={context.handleContact}
-              name="tag2"
-              placeholder="Tag 2"
-            />
+                    <input
+                    value={context.contact.tag2}
+                    onChange={context.handleContact}
+                    name="tag2"
+                    placeholder="Tag 2"
+                    />
 
-            <input
-              value={context.contact.tag3}
-              onChange={context.handleContact}
-              name="tag3"
-              placeholder="Tag 3"
-            />
+                    <input
+                    value={context.contact.tag3}
+                    onChange={context.handleContact}
+                    name="tag3"
+                    placeholder="Tag 3"
+                    />
 
-            <input
-              value={context.contact.tag4}
-              onChange={context.handleContact}
-              name="tag4"
-              placeholder="Tag 4"
-            />
-            </form>
-            </div>
-            <div id="contact-card-right">
-            <form onSubmit={handleContactSubmit}>
+                    <input
+                    value={context.contact.tag4}
+                    onChange={context.handleContact}
+                    name="tag4"
+                    placeholder="Tag 4"
+                    />
+                </form>
+            <div id="contact-card-right-grid">
+                <form onSubmit={handleContactSubmit}>
                 <h1> Notes</h1>
-            <input
-              value={context.contact.notes}
-              onChange={context.handleContact}
-              name="notes"
-              placeholder="Notes"
-            />
-            <div>
-            <input id="contact-contact-btn-save" type="submit" value="SAVE" />
+                        <input
+                        value={context.contact.notes}
+                        onChange={context.handleContact}
+                        name="notes"
+                        placeholder="Notes"
+                        />
+                    <div className="card_right">
+                        <img src={contact_right} alt="card_right" className="card_right" />
+                        <img src={contact_rightdown} alt="card_right" className="card_right" />
+                    </div>         
+                </form>
             </div>
-            
-          </form>
-          </div>
           </div>
         </div>
       </div>
