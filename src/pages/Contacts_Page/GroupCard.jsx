78d3f712f35b5f72d3ee650caf4f8425605d0059
jessspo <67518904+jessspo@users.noreply.github.com>
2022-07-19 16:13:32 +0200

    costumizedinputbox

--- src/pages/Contacts_Page/GroupCard.jsx
+++ src/pages/Contacts_Page/GroupCard.jsx
@@ -1,5 +1,6 @@
 
 import './GroupCard.css'
+import CostumizedInputBox from '../../components/CostumizedInputBox/CostumizedInputBox';
 
 const GroupCard = () => {
 
@@ -7,6 +8,7 @@ const GroupCard = () => {
 
     return (
         <div className='group_card_container'>
+            <CostumizedInputBox />
             <div className='gc_image_container'>
             </div>
             <h1>This is the group card component</h1>
