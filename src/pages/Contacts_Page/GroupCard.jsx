10a88e07b0b5e6c7408b50fc737968423e08c84d
jessspo <67518904+jessspo@users.noreply.github.com>
2022-07-19 16:33:48 +0200

    layout view card page

--- src/pages/Contacts_Page/GroupCard.jsx
+++ src/pages/Contacts_Page/GroupCard.jsx
@@ -1,18 +1,21 @@
 
 import './GroupCard.css'
 import CostumizedInputBox from '../../components/CostumizedInputBox/CostumizedInputBox';
+import Layout from '../../components/Layout/Layout';
 
 const GroupCard = () => {
 
 //Todo this component size, shape and position can be defined, then I will add the content. If you need to check how then interact on the page just render multiple components
 
     return (
+
         <div className='group_card_container'>
             <CostumizedInputBox />
             <div className='gc_image_container'>
             </div>
             <h1>This is the group card component</h1>
         </div>
+
     )
 };
 
